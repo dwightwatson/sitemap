99350030fe9b77388c1c0429e75527ef70ffef78
Dave Carlson <davecarlson@users.noreply.github.com>
2016-01-24 22:35:58 +0000

    Use full echo syntax

--- src/views/sitemap.php
+++ src/views/sitemap.php
@@ -1,4 +1,4 @@
-<?= '<?xml version="1.0" encoding="UTF-8"?>' ?>
+<?php echo '<?xml version="1.0" encoding="UTF-8"?>' ?>
 <urlset
   xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
   xmlns:image="http://www.google.com/schemas/sitemap-image/1.1"
@@ -7,20 +7,20 @@
     http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
   <?php foreach ($tags as $tag): ?>
     <url>
-      <loc><?= $tag->getLocation() ?></loc>
+      <loc><?php echo $tag->getLocation() ?></loc>
       <?php if ($tag->getLastModified()): ?>
-        <lastmod><?= $tag->getLastModified()->format('Y-m-d\TH:i:sP') ?></lastmod>
+        <lastmod><?php echo $tag->getLastModified()->format('Y-m-d\TH:i:sP') ?></lastmod>
       <?php endif ?>
       <?php if ($tag instanceof \Watson\Sitemap\Tags\Tag): ?>
         <?php if ($tag->getPriority()): ?>
-          <priority><?= $tag->getPriority() ?></priority>
+          <priority><?php echo $tag->getPriority() ?></priority>
         <?php endif ?>
         <?php if ($tag->getChangeFrequency()): ?>
-          <changefreq><?= $tag->getChangeFrequency() ?></changefreq>
+          <changefreq><?php echo $tag->getChangeFrequency() ?></changefreq>
         <?php endif ?>
       <?php endif; ?>
       <?php if ($tag instanceof \Watson\Sitemap\Tags\ExpiredTag): ?>
-        <expires><?= $tag->getExpired()->format('Y-m-d\TH:i:sP') ?></expires>
+        <expires><?php echo $tag->getExpired()->format('Y-m-d\TH:i:sP') ?></expires>
       <?php endif; ?>
     </url>
   <?php endforeach ?>
